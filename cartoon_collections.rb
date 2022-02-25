39708c1b807b2a4659ebb8fe005e6d88bd00d7c5
Zach <zawood11@gmail.com>
2022-02-25 01:06:24 -0500

    completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.map.with_index{|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |name|
+    name[0] = name[0].upcase 
+    name + '!'
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  long_calls = array.filter{|calls| calls.length > 4}
+  long_calls.length > 0 ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |food|
+    cheese_types.include?(food)
+  end
 end
